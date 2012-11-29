aa563b8a9afd391f974382a97298c291ca43f218
v mos <vmosley@icstars.org>
2012-11-28 18:28:59 -0600

    Logic fixed for toggle first second and third(output). Code serverly reduced. will fix additional problems.

--- js/toggleOutputs.js
+++ js/toggleOutputs.js
@@ -1,15 +1,15 @@
- //[type=tel] can replace [type=tel]
- //	$('input[type=text]:not([data-x=1])')
- //$("[type=tel][data-x='3']").each(function() { do something } );    //just for reference
-	//when padding is 100 clear and reset padding
-
+//[type=tel] can replace [type=tel]
+//	$('input[type=text]:not([data-x=1])')
+//$("[type=tel][data-x='3']").each(function() { do something } );    //just for reference
+//when padding is 100 clear and reset padding
+//$("input[name=AA]").addClass("selected");
 	
 /* Clear swiped Input plus all calculated Inputs */
 $('[type=tel]').bind('swipeleft',function(){
 	$('[type=tel][data-x="3"]').stop().animate({'padding-right':800},'slow');
 	$(this).stop().animate({'padding-right':800},'slow',function(){
-		$(this).css({'padding-right':0});
-		$('[type=tel][data-x="3"]').css({'padding-right':0});
+		$(this).css({'padding-right':10});
+		$('[type=tel][data-x="3"]').css({'padding-right':10});
 		
 		$(this).val('');
 		$('[type=tel][data-x="3"]').val('');
@@ -18,27 +18,29 @@ $('[type=tel]').bind('swipeleft',function(){
 
 // NEW CODE***************************************************************
 // NEW CODE***************************************************************
+$('[type=tel]').change(function() {
+ $(this).css({color:"black"});
+ 
+});
+
 $('[type=tel]').bind(' keyup change',function(){
-	$(this).css({color:"black"});
+	
 	//var count = 0;
 	//count++;
 
-	//$("input[name=AA]").addClass("selected");
+	
 
  	//Get type of output/input, 1,2 or 3 (real)
-	 dataCurrent= $(this).attr('data-x');
+	 dataCurrent = $(this).attr('data-x');
 	
-	if (dataCurrent != '1'){   //if not input(1) set to input(1) and set rest
+	if (dataCurrent == '3'){  
+			$(this).attr('data-x','1');
+			$('[type=tel][data-x=2]').not(this).attr('data-x','3');
+			$('[type=tel][data-x=1]').not(this).attr('data-x','2');
+	}else if (dataCurrent == '2'){
 		$(this).attr('data-x','1');
-		if($("[type=tel]").not(this) == '1' )
-			$("[type=tel]").not(this).attr('data-x','2');
-			else if($("[type=tel]").not(this) == '2')
-				$("[type=tel]").not(this).attr('data-x','3');		
-	}
-	else if (dataCurrent == '1') //Reset Input Color -> probably should do nothing
-	{
-	//do nothing
-	}
+		$('[type=tel][data-x=1]').not(this).attr('data-x','2');
+	}else{/*Add error checking for version 2, with a data-x counter. if != default then reset*/}
 	
 	//Get type of output/input, 1,2 or 3 (real)
 	dataBillRate = $("[type=tel][name='billRate']").attr('data-x');
@@ -54,14 +56,11 @@ $('[type=tel]').bind(' keyup change',function(){
     grossProfitMargin 	= 23;
 	grossMarginPercent	= 23;
 	
-	//Change Style for Outputs
-	$('[type=tel][data-x=3]').css({color:"red"});	
-	
-	
-	$(':input[name="billRate"]').val(billRate.toString());
-	$(':input[name="payRate"]').val(payRate.toString());
-	$(':input[name="markUp"]').val(markUp.toString());
-	$(':input[name="grossProfitMargin"]').val(grossProfitMargin.toString());
-	$(':input[name="grossMarginPercent"]').val(grossMarginPercent.toString());
+		
+	$('[type=tel][name="billRate"]').val(billRate.toString());
+	$('[type=tel][name="payRate"]').val(payRate.toString());
+	$('[type=tel][name="markUp"]').val(markUp.toString());
+	$('[type=tel][name="grossProfitMargin"]').val(grossProfitMargin.toString());
+	$('[type=tel][name="grossMarginPercent"]').val(grossMarginPercent.toString());
 
 });	
