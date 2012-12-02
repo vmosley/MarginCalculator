739517e64a371e2b12155a27aef17fbcdc780be5
v mos <vmosley@icstars.org>
2012-12-01 20:19:02 -0600

    updates, still issues

--- js/toggleOutputs.js
+++ js/toggleOutputs.js
@@ -3,11 +3,14 @@
 //$("[type=tel][data-x='3']").each(function() { do something } );    //just for reference
 //when padding is 100 clear and reset padding
 //$("input[name=AA]").addClass("selected");
+
+
 	
 /* Clear swiped Input plus all calculated Inputs */
 $('[type=tel]').bind('swipeleft',function(){
-	$('[type=tel][data-x="3"]').stop().animate({'padding-right':700},'slow');
-	$(this).stop().animate({'padding-right':700},'slow',function(){
+	$('[type=tel][data-x="3"]').stop().animate({'padding-right':250},'fast');
+	//$('[type=tel][data-x="3"]').animate({color:'white'},250);
+	$(this).stop().animate({'padding-right':250},'fast',function(){
 		$(this).css({'padding-right':10});
 		$('[type=tel][data-x="3"]').css({'padding-right':10});
 		
@@ -16,28 +19,38 @@ $('[type=tel]').bind('swipeleft',function(){
 	});
 });
 
-// NEW CODE***************************************************************
-// NEW CODE***************************************************************
 
-
+$("[type=tel]").focus(function() {
+		$(this).addClass("curFocus");
+	});
+	
+$("[type=tel]").blur(function() {
+		$(this).removeClass("curFocus");
+	});
+// NEW CODE***************************************************************
 $('[type=tel]').bind(' keydown ',function(){
-		//if(isNaN(C)){} 
+		
+	
+	
 	
 	$(this).css({color:"black"});
 	$('[type=tel][data-x=3]').css({color:"red"});
 	//var count = 0;
 	//count++;
+	 
+	dataCurrent = $(this).attr('data-x');
  	//Get type of output/input, 1,2 or 3 (real)
-	 dataCurrent = $(this).attr('data-x');
 	
 	if (dataCurrent == '3'){  
-			$(this).attr('data-x','1');
-			$('[type=tel][data-x=2]').not(this).attr('data-x','3');
-			$('[type=tel][data-x=1]').not(this).attr('data-x','2');
+		$(this).attr('data-x','1');
+		$('[type=tel][data-x=2]').not(this).attr('data-x','3');
+		$('[type=tel][data-x=1]').not(this).attr('data-x','2');
 	}else if (dataCurrent == '2'){
 		$(this).attr('data-x','1');
 		$('[type=tel][data-x=1]').not(this).attr('data-x','2');
 	}else{/*Add error checking for version 2, with a data-x counter. if != default then reset*/}
+	
+	
 	
 	//Get type of output/input, 1,2 or 3 (real)
 	dataBillRate = $("[type=tel][name='billRate']").attr('data-x');
@@ -52,6 +65,7 @@ $('[type=tel]').bind(' keydown ',function(){
     grossProfitMargin 	= 23;
 	grossMarginPercent	= (grossProfitMargin / billRate);
 	
+	$('[type=tel][data-x=3]').addClass("equals");
 		
 	$('[type=tel][name="billRate"][data-x=3]').not(this).not('[data-x=2]').val(billRate.toString());
 	$('[type=tel][name="payRate"][data-x=3]').not(this).not('[data-x=2]').val(payRate.toString());
@@ -59,8 +73,12 @@ $('[type=tel]').bind(' keydown ',function(){
 	$('[type=tel][name="grossProfitMargin"][data-x=3]').not(this).not('[data-x=2]').val(grossProfitMargin.toString());
 	$('[type=tel][name="grossMarginPercent"][data-x=3]').not(this).not('[data-x=2]').val(grossMarginPercent.toString());
 
+	
+
 });	
 
+
+/* //function grabbed from web to remove NaN
 function recalc() {
   jQuery("input[class^=percent]").calc("(invoice - cost)/invoice * 100", {
     invoice: jQuery("input[class^=invoice]"),
@@ -70,4 +88,4 @@ function recalc() {
     // return an empty string if s is NaN
     return !isNaN(s) ? s.toFixed(2) + "%" : "";
   });
-}
+} */
